37d8ee12a92d32afbc698dc7ed0cfcea4e0338b4
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2026-07-15 15:32:21 +0530

    4th pattern

---
+++ 4th.js
@@ -0,0 +1,13 @@
+function pattern3(n) {
+        let i,j
+        for(i=1; i<=n; i++){
+            let row = '';
+            for(j=1; j<=i; j++ ){
+
+            row += i+ " ";
+
+            }
+        console.log(row)
+        }
+    }
+pattern3(4)
