9e9e355cbc7b141864cf6d2fdd999064e799e703
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2026-07-15 16:18:54 +0530

    6th pattern

---
+++ 6th.js
@@ -0,0 +1,11 @@
+function pattern6(n){
+    let i,j;
+    for (i=1 ; i<=n; i++){
+        let row = ''
+        for(j=1; j<= n-i+1; j++){
+            row += j
+        }
+        console.log(row)
+    }
+}
+pattern6(5)
