a09fa07cf8db4d283671297f809f4aeea3124e0b
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2026-07-15 17:17:28 +0530

    8th patter

---
+++ 8th.js
@@ -0,0 +1,17 @@
+function pattern8(n){
+    let i,j,k,l;
+    for(i=1; i<=n; i++){
+        let row = '';
+        for(j=1;j<=i;j++){
+            row += ' '
+        }
+        for(k=0; k<= n-i; k++){
+            row += '*'
+        }
+        for(l=1; l<= n-i; l++){
+            row +='*'
+        }
+        console.log(row)
+    }
+}
+pattern8(5)
