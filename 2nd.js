99a2f1896f19efff128c0d926d8873071c255a4a
Akshay Chandel <139372635+akshaychandel1@users.noreply.github.com>
2026-07-15 12:51:22 +0530

    2nd pattern

---
+++ 2nd.js
@@ -0,0 +1,12 @@
+ function pattern2(n){
+        let i,j;
+        for(i=1; i<=n; i++){
+            let row = ''
+            for(j=1; j<=i; j++){
+                row += '*'
+            }
+                console.log(row)
+        }
+    }
+ 
+    pattern2(4);
